55255104f1fa62e565b47b51c4ee2f9887f87be9
nzonic <zonic.nikola@gmail.com>
2021-06-21 19:37:44 +0200

    Added Basic Layout and Register page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <router-link class="navbar-brand" to="/">Galleries</router-link>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Home</router-link>
+        </li>
+        <!-- <li class="nav-item">
+          <router-link class="nav-link" to="/login">Link</router-link>
+        </li> -->
+      </ul>
+        <router-link class="btn btn-outline-primary" to="/login">Login</router-link>
+        <router-link class="btn btn-outline-primary ml-2" to="/register">Register</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <b-form @submit.stop.prevent="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+      
+      <b-form-group
+        id="input-group-2"
+        label="Password:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          placeholder="Enter password"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-3"
+        label="Confirm Password:"
+        label-for="input-3"
+      >
+        <b-form-input
+          id="input-3"
+          v-model="form.password_confirmation"
+          type="password"
+          placeholder="Enter password again"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4" label="Your First Name:" label-for="input-4">
+        <b-form-input
+          id="input-4"
+          v-model="form.firstName"
+          placeholder="Enter First name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-5" label="Your Last Name:" label-for="input-5">
+        <b-form-input
+          id="input-5"
+          v-model="form.lastName"
+          placeholder="Enter Last name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+
+      <b-form-checkbox
+        id="checkbox-1"
+        v-model="form.status"
+        name="checkbox-1"
+        value="accepted"
+        unchecked-value="not_accepted"
+      >
+        I accept the terms and conditions.
+      </b-form-checkbox>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          firstName: '',
+          lastName: '',
+          password: '',
+          password_confirmation: '',
+          status: 'not_accepted'
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(event) {
+        event.preventDefault()
+        this.form.email = ''
+        this.form.firstName = ''
+        this.form.lastName = ''
+        this.form.password = ''
+        this.form.password_confirmation = ''
+        this.form.status = 'not_accepted'
+
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
